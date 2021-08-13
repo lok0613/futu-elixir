cddb78484ef364181e75328823d428a827fe0983
Lok Chun Wai <cwlok0613@gmail.com>
2021-08-14 02:13:24 +0800

    Fix version in tests

--- test/futu/basic/init_connect_test.exs
+++ test/futu/basic/init_connect_test.exs
@@ -5,7 +5,7 @@ defmodule Futu.Basic.InitConnectTest do
 
   test "map_c2s/1" do
     assert [
-             clientVer: 100,
+             clientVer: 110,
              clientID: "FutuElixir",
              recvNotify: false,
              programmingLanguage: "Elixir"
