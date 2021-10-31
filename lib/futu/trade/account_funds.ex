a179bda4c53fa1d4eae1424643ce53df48777cec
Lok Chun Wai <cwlok0613@gmail.com>
2021-10-31 22:41:36 +0800

    Trade API

---
+++ lib/futu/trade/account_funds.ex
@@ -0,0 +1,7 @@
+defmodule Futu.Trade.AccountFunds do
+  @moduledoc false
+  use Futu.Component.Api
+
+  proto_id 2101
+  proto_module Trd_GetFunds
+end
---
+++ lib/futu/trade/history_order_list.ex
@@ -0,0 +1,7 @@
+defmodule Futu.Trade.HistoryOrderList do
+  @moduledoc false
+  use Futu.Component.Api
+
+  proto_id 2221
+  proto_module Trd_GetHistoryOrderList
+end
