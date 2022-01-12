30fb0e1f41f771796db3604ef06615faeb8f7db9
Lok Chun Wai <cwlok0613@gmail.com>
2022-01-13 02:09:04 +0800

    Bump version to v2.4.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Futu.MixProject do
   def project do
     [
       app: :futu,
-      version: "2.4.1",
+      version: "2.4.2",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
--- test/futu/basic/init_connect_test.exs
+++ test/futu/basic/init_connect_test.exs
@@ -5,7 +5,7 @@ defmodule Futu.Basic.InitConnectTest do
 
   test "map_c2s/1" do
     assert [
-             clientVer: 241,
+             clientVer: 242,
              clientID: "FutuElixir",
              recvNotify: false,
              programmingLanguage: "Elixir"
