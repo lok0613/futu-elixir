809d3310e704dd2a565e54521280b58aea107994
Lok Chun Wai <cwlok0613@gmail.com>
2022-01-23 15:39:34 +0800

    2.5.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Futu.MixProject do
   def project do
     [
       app: :futu,
-      version: "2.4.2",
+      version: "2.5.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
