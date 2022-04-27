0ff47939ebc32c5609242473f367462390871863
Lok Chun Wai <cwlok0613@gmail.com>
2022-04-27 13:48:02 +0800

    Bump version to v2.7.1

--- test/futu/basic/init_connect_test.exs
+++ test/futu/basic/init_connect_test.exs
@@ -5,7 +5,7 @@ defmodule Futu.Basic.InitConnectTest do
 
   test "map_c2s/1" do
     assert [
-             clientVer: 270,
+             clientVer: 271,
              clientID: "FutuElixir",
              recvNotify: false,
              programmingLanguage: "Elixir"
