b6bca19b659a87e4cb092e470451aa851859ff11
Lok Chun Wai <cwlok0613@gmail.com>
2022-05-02 22:44:01 +0800

    Bump version to v2.8.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Futu.MixProject do
   def project do
     [
       app: :futu,
-      version: "2.7.1",
+      version: "2.8.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
